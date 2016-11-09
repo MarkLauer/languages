d0da140bfb2e79b9ce1df1c00d760f5a3996cd1f
MarkLauer <allright96@gmail.com>
2016-11-09 22:57:27 +0700

    Updated again

--- spammers.py
+++ spammers.py
@@ -8,7 +8,7 @@ def goin(url, n):
         r = requests.get(url)
         _dict[url] = 1
         html = r.text
-        emails.extend(re.findall(r'\b[\w.-]+@[\w.-]+\.\w+', html))
+        emails.extend(re.findall(r'[\w.-]+@[\w.-]+\.\w+', html))
         valid_urls = set(re.findall(home + r'[\w./]+', html))
         for item in valid_urls:
             if _dict.get(item) == 1:
