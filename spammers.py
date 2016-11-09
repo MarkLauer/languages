392b1143ae016dcb790241262021af95fdaea2cb
MarkLauer <allright96@gmail.com>
2016-11-09 22:55:48 +0700

    Updated

--- spammers.py
+++ spammers.py
@@ -1,16 +1,14 @@
 import re
 import requests
-home = 'http://www.mail.ru/'
+home = 'http://www.mosigra.ru/'
 emails = []
 _dict = {}
-valid_urls = set()
 def goin(url, n):
     if n > 0:
         r = requests.get(url)
         _dict[url] = 1
         html = r.text
-        emails.extend(re.findall(r'[\w.-]+@[\w.-]+\.\w+', html))
-        global valid_urls
+        emails.extend(re.findall(r'\b[\w.-]+@[\w.-]+\.\w+', html))
         valid_urls = set(re.findall(home + r'[\w./]+', html))
         for item in valid_urls:
             if _dict.get(item) == 1:
