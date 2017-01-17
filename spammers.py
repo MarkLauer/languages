dad4b9bc242641ebdf372d5667300b901180f19d
MarkLauer <allright96@gmail.com>
2017-01-17 12:56:39 +0700

    last update

--- spammers.py
+++ spammers.py
@@ -2,18 +2,25 @@ import re
 import requests
 home = 'http://www.mosigra.ru/'
 emails = []
-_dict = {}
-def goin(url, n):
-    if n > 0:
+visitedUrls = {}
+c = 10 #количество переходов
+def find(url):
+    global c
+    global emails
+    global visitedUrls
+    if c > 0:
+        c = c - 1
         r = requests.get(url)
-        _dict[url] = 1
+        visitedUrls[url] = 1
         html = r.text
-        emails.extend(re.findall(r'[\w.-]+@[\w.-]+\.\w+', html))
-        valid_urls = set(re.findall(home + r'[\w./]+', html))
-        for item in valid_urls:
-            if _dict.get(item) == 1:
+        emails.extend(re.findall(r'mailto:[\w.-]+@[\w.-]+\.\w+', html))
+        validUrls = set(re.findall('<a href=[\'\"]' + home + r'[\w./]+', html))
+        for item in validUrls:
+            if visitedUrls.get(item[9:]) == 1:
                 continue
             else:
-                goin(item, n - 1)
-goin(home, 2)
+                find(item[9:])
+find(home)
+for i in range(len(emails)):
+    emails[i] = emails[i][7:]
 print (set(emails))
