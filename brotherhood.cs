9db5592f91dfa6961176e0895270870bb1c4abb3
MarkLauer <allright96@gmail.com>
2017-01-18 23:06:11 +0700

    updated

--- brotherhood.cs
+++ brotherhood.cs
@@ -1,47 +1,49 @@
 using System;
 using System.Threading;
 
-public class Brotherhood
+public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        int N = 20;
-        Token token = new Token("some data", N);
-        for (int i = 1; i <= N; i++)
+        int n = 20;
+        Token token = new Token();
+        token.data = "data";
+        token.recipient = n;
+        ThreadRunner runner = new ThreadRunner();
+        runner.Run(token, n);
+    }
+}
+
+public class ThreadRunner
+{
+    private Token token;
+
+    public void Run(Token token, int n)
+    {
+        this.token = token;
+        for (int i = 1; i <= n; i++)
         {
-            token.setCurrent(i);
-            Thread newThread = new Thread(Brotherhood.Func);
-            newThread.Start(token);
+            Thread thread = new Thread(Func); 
+            thread.Start(i);
         }
+        
     }
 
-    public static void Func(object data)
+    private void Func(object data)
     {
-        Token token = (Token) data;
-        if (token.getRecipient() == token.getCurrent())
+        lock (token)
         {
-            Console.WriteLine("arrived at thread #" + token.getCurrent());
+            int threadNum = (int)data;
+            Console.WriteLine("thread #{0}", threadNum);
+            if (token.recipient == threadNum) {
+                Console.WriteLine("{0} received", token.data);
+            }
         }
     }
 }
 
 public class Token
 {
-    private string data;
-    private int recipient;
-    private int current;
-
-    public Token(string data, int recipient)
-    {
-        this.data = data;
-        this.recipient = recipient;
-    }
-
-    public void setCurrent(int i) { current = i; }
-
-    public string getData() { return data; }
-
-    public int getRecipient() { return recipient; }
-
-    public int getCurrent() { return current; }
+    public string data;
+    public int recipient;
 }
